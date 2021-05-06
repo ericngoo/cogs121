91b71b8f256817220c2304638464a56ac897367d
Eric Ngo <engo1207@gmail.com>
2021-05-05 20:03:08 -0700

    fixed dependencies

--- app.js
+++ app.js
@@ -3,43 +3,19 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var handlebars = require('express3-handlebars')
+const express = require('express');
 
-var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
+const app = express();
+app.use(express.static("public"));
 
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-// Add routes here
-app.get('/', index.view);
-app.get('/ez-audit', index.audit);
-// Example route
-// app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
 });
+
+app.get("/ez-audit", (req, res) => {
+  res.sendFile(__dirname + "/views/audit.html")
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+})
